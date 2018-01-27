5f62ca426c687ec882c66e61cf35fa29f381996e
Spencer Mycek <Spence.Mycek@gmail.com>
2018-01-27 15:19:22 -0500

    Improve index page with templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Spencer'}
+    todos = [
+        {
+            'author': {'username': 'Spencer'},
+            'body': 'Make dentist appointment',
+            'due_date': '2/1/18'
+        },
+        {
+            'author': {'username': 'Spencer'},
+            'body': 'Finish Physics homework',
+            'due_date': '1/29/18'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, todos=todos)
