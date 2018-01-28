4e460c307de9ff0cbd818622c62738be7192e605
Spencer Mycek <Spence.Mycek@gmail.com>
2018-01-27 21:58:25 -0500

    Fix avatar function, add unit testing

--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://ww.gravatar.com/avatar.com/avatar/{}'.format(
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
     def all_todos(self):
