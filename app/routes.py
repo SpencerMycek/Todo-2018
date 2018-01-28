e2de1c5ae067a3b76d1518997479ef2fb00bcb3e
Spencer Mycek <Spence.Mycek@gmail.com>
2018-01-27 20:19:36 -0500

    Complete login/logout functionality

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Spencer'}
     todos = [
         {
             'author': {'username': 'Spencer'},
@@ -21,7 +22,7 @@ def index():
             'due_date': '1/29/18'
         }
     ]
-    return render_template('index.html', title='Home', user=user, todos=todos)
+    return render_template('index.html', title='Home', todos=todos)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -30,16 +31,19 @@ def login():
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.date).first()
+        user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('index'))
+            return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
-@app.rout('/logout')
+@app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
