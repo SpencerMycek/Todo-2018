45be83c1b934a7d62537105c5b2141026729c572
Spencer Mycek <Spence.Mycek@gmail.com>
2018-01-27 21:32:36 -0500

    Add file logging

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+import os
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -28,6 +29,17 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/todo.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('To-Do Startup')
 
 """
 To use your Gmail account's email server:
