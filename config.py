841171157a3e68ff8506c20cc96e952497751d5e
Spencer Mycek <Spence.Mycek@gmail.com>
2018-03-04 15:25:20 -0500

    Create a .env for environment variables and allow flask to use them

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Dummy-String'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Event Shelter Belt Wipe'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFCATIONS = False
@@ -12,7 +15,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_T:S') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['zilla@csh.rit.edu']
     TODOS_PER_PAGE = 5
     TODOS_PER_ALL = 15
     LANGUAGES = ['en,' 'es']
