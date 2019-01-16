61c366ea75104aec5683d75e8f42c9985d1a0109
Spencer Mycek <Spence.Mycek@gmail.com>
2019-01-16 11:12:41 -0500

    Fix typo in environment variable

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Event Shelter Belt Wipe'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFCATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_T:S') is not None
